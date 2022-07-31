1fc4b2ac91376546a36beb8ffa08e0596ae6358e
jamspoon <jamspoonart@gmail.com>
2022-07-30 23:32:37 -0500

    hide normal ui in ww

--- scripts/walterswares/MainMenu.gd
+++ scripts/walterswares/MainMenu.gd
@@ -3,4 +3,5 @@ extends Control
 export(AudioStream) var bgm
 
 func _ready():
+	EventBus.emit_signal("ui_visibility_changed", false)
 	EventBus.emit_signal("bgm_changed", {"playing": true, "stream": bgm})
