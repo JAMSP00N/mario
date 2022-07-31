4e0f7ab4efe81c4c0ca314e6868feefc4f68546e
jamspoon <jamspoonart@gmail.com>
2022-07-31 13:15:46 -0500

    theme label

--- scripts/walterswares/MapRegion.gd
+++ scripts/walterswares/MapRegion.gd
@@ -2,12 +2,16 @@ extends Area2D
 
 export(String, DIR) var microgame_folder
 export(Texture) var select_sprite
+export(String) var title
+export(Color) var color
 
 var highlighted: bool
 
 var parent
+var title_label
 func _ready():
 	parent = get_parent()
+	title_label = get_parent().get_parent().get_node("title_label")
 	highlighted = false
 	pass
 
@@ -15,11 +19,13 @@ func _on_area_entered(area):
 	highlighted = true
 	if select_sprite:
 		parent.texture = select_sprite
+	title_label.bbcode_text = "\n[right][wave][color=#" + color.to_html(false) + "]" + tr(title) + " [/color][/wave][/right]"
 
 func _on_area_exited(area):
 	highlighted = false
 	if parent.default_sprite:
 		parent.texture = parent.default_sprite
+	title_label.bbcode_text = ""
 
 func _input(event):
 	if highlighted && event is InputEventMouseButton && event.pressed:
